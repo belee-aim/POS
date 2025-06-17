a9174e684a94174720e03e0651439fc4adcf3214
tugushka <battulga@aim.mn>
2025-06-17 02:46:54 +0000

    Init pos

---
+++ pos/public/js/pos.bundle.js
@@ -0,0 +1,8 @@
+import "../../pos/page/pos/pos_item_selector.js";
+import "../../pos/page/pos/pos_item_cart.js";
+import "../../pos/page/pos/pos_item_details.js";
+import "../../pos/page/pos/pos_number_pad.js";
+import "../../pos/page/pos/pos_payment.js";
+import "../../pos/page/pos/pos_past_order_list.js";
+import "../../pos/page/pos/pos_past_order_summary.js";
+import "../../pos/page/pos/pos_controller.js";
